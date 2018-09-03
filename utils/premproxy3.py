5cd2cb7d397defa1b02d5305bbe0aafea7f84ec7
dusk <exinyijiu@163.com>
2018-09-03 09:55:50 +0800

    ‘去掉异步爬虫注释的’

--- utils/premproxy3.py
+++ utils/premproxy3.py
@@ -3,8 +3,15 @@ import asyncio
 from lxml import etree
 import jsbeautifier
 import re
+import requests
 import sys
 import queue
+import threading
+import os
+
+if os.path.exists("proxyver.txt"):
+    os.remove("proxyver.txt")
+
 
 
 headers = {"Host": "premproxy.com",
@@ -12,12 +19,25 @@ headers = {"Host": "premproxy.com",
                "Referer": "https://premproxy.com"
                }
    
+lock = threading.Lock()
+
 q = queue.Queue()
 
 
-async def ver(q):
-    while not q.empty():
-        print(q.qsize())
+def verpro(q):
+    vc = q.get(timeout=0.2)
+    proxies = {"http":vc}
+    try:
+        tv = requests.get("http://www.freebuf.com",proxies=proxies,timeout=6)
+        if tv.status_code == 200:
+            with lock:
+                with open("proxyver.txt","a+") as ww:
+                    ww.write(vc + '\n')
+                    ww.flush()
+    except Exception as e:
+        pass
+
+
 
 async def get(url,call_parse=None):
     
@@ -26,25 +46,37 @@ async def get(url,call_parse=None):
             txt = await resp.text()
             stu = resp.status
             if stu == 404:
-                print(q.qsize())
+                print("爬取完毕")
+                while not q.empty():
+                    th = []
+                    for i in range(20):
+                        thd = threading.Thread(target=verpro,args=(q,))
+                        th.append(thd)
+                    for i in th:
+                        if not i.isAlive():
+                            i.start()
+                    for h in th:
+                        h.join()
+                           
+                print("队列大小{}".format(q.qsize()))
                 sys.exit()
             p = etree.HTML(txt)
             ipp = p.xpath('//*[@class="anon" or @class="transp"]/td[1]/text()')
             jquery = "https://premproxy.com" + re.findall(r'<script src="(.*?)"></script>',txt)[1]
             async with session.get(jquery) as rejq:
                 jqcont = await rejq.text()
-                jstu = rejq.status
-                if jstu == 404:
-                    print("js404")
-                    while not q.empty():
-                        pp = q.get()
-                        try:
-                            async with session.get("http://www.proxy.com",proxy = "http://{}".format(pp),timeout=5) as repr:
-                                if repr.status == 200:
-                                    print("代理可用:{}".format(pp))
-                        except Exception as e:
-                            print("代理不可用:{}".format(pp))
-                            continue
+                #jstu = rejq.status
+                #if jstu == 404:
+                #    print("js404")
+                #    while not q.empty():
+                #        pp = q.get()
+                #        try:
+                #            async with session.get("http://www.proxy.com",proxy = "http://{}".format(pp),timeout=5) as repr:
+                #                if repr.status == 200:
+                #                    print("代理可用:{}".format(pp))
+                #        except Exception as e:
+                #            print("代理不可用:{}".format(pp))
+                #            continue
                 beau = jsbeautifier.beautify(jqcont)
                 for i in ipp:
                     ip = i.replace(":","").strip()
@@ -52,8 +84,7 @@ async def get(url,call_parse=None):
                     try:
                         pot =  re.findall('\.'+po+'\\\\\'\)\.html\((.*?)\)',beau)[0].strip()
                     except Exception as e:
-                        print(e)
-                        pot  = ""
+                        pot  = "8080"
                     v = i + pot
                     print(v)
                     q.put(v)
