13f8142b7e7e19121f499bed7510b56ac8f29dce
dusk <exinyijiu@163.com>
2018-09-05 16:25:10 +0800

    多线程协程完善好

--- utils/premproxy3.py
+++ utils/premproxy3.py
@@ -8,6 +8,7 @@ import sys
 import queue
 import threading
 import os
+import time
 
 if os.path.exists("proxyver.txt"):
     os.remove("proxyver.txt")
@@ -44,24 +45,6 @@ async def get(url,call_parse=None):
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as resp:
             txt = await resp.text()
-            stu = resp.status
-            if stu == 404:
-                print("爬取完毕")
-                while not q.empty():
-                    th = []
-                    
-                    for i in range(q.qsize()):
-                        print("线程{}".format(i))
-                        thd = threading.Thread(target=verpro,args=(q,))
-                        th.append(thd)
-                    for i in th:
-                        if not i.isAlive():
-                            i.start()
-                    for h in th:
-                        h.join()
-                           
-                print("队列大小{}".format(q.qsize()))
-                sys.exit()
             p = etree.HTML(txt)
             ipp = p.xpath('//*[@class="anon" or @class="transp"]/td[1]/text()')
             jquery = "https://premproxy.com" + re.findall(r'<script src="(.*?)"></script>',txt)[1]
@@ -76,7 +59,6 @@ async def get(url,call_parse=None):
                     except Exception as e:
                         pot  = "8080"
                     v = i + pot
-                    print(v)
                     q.put(v)
        
 
@@ -85,13 +67,36 @@ def parse_url():
 
 
 if __name__ == "__main__":
+    tstart = time.time()
     i = 1
+    qmax = []
     u = "https://premproxy.com/list/{}.htm"
+    print("开始爬取页数")
     while True:
         query = str(i).zfill(2) if i <10 else i
         url = u.format(query)
-        print("第{}页".format(query))
-        result = asyncio.ensure_future(get(url)) 
-        ev = asyncio.get_event_loop()
-        ev.run_until_complete(result)
+        if requests.head(u.format(query)).status_code == 404:
+            break
+        else:
+            qmax.append(u.format(query))
         i += 1
+    print("爬取页数为: {}".format(len(qmax)))
+    print("开始爬取代理")
+    result = [ asyncio.ensure_future(get(u)) for u in qmax ] 
+    ev = asyncio.get_event_loop()
+    ev.run_until_complete(asyncio.gather(*result))
+    ev.close()
+    print("爬取代理数为: {}".format(q.qsize()))
+    print("开始检测可用代理")
+    while not q.empty():
+        th = []
+        for i in range(q.qsize()):
+            thd = threading.Thread(target=verpro,args=(q,))
+            th.append(thd)
+        for i in th:
+            i.start()
+        for h in th:
+            h.join()
+    print("检测可用代理完成!")
+    tend = time.time()
+    print("共计花费时间: {}",format(tend-tstart))
